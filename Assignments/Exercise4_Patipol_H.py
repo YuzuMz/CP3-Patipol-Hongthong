53b675cca3098657687fd769b99f44aec14849f2
YuzuMz <patipolz.15@gmail.com>
2026-06-03 16:53:07 +0700

    Add files via upload

---
+++ Assignments/Exercise4_Patipol_H.py
@@ -0,0 +1,22 @@
+FE   = float(input("Founddation English Score : "))
+GB   = float(input("General Business Score : "))
+ITCS = float(input("Introduction to Computer Systems Score : "))
+CP   = float(input("Computer Programming Score : "))
+
+print("----- Your Score -----")
+print("Founddation English Score :        ",FE,"         Score")
+print("General Business Score :           ",GB,"         Score")
+print("Introduction to Computer Systems : ",ITCS,"         Score")
+print("Foundation English :               ",CP,"         Score")
+print("------------------------------------------------------------")
+print("---------- Total : ",FE+GB+ITCS+CP,"Score ----------")
+
+
+
+
+
+
+
+
+
+
