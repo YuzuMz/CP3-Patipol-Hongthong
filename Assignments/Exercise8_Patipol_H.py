90bb5a0feca1d78b0a55eaebcce5af30cb3160c5
YuzuMz <patipolz.15@gmail.com>
2026-06-04 22:20:02 +0700

    Add files via upload

---
+++ Assignments/Exercise8_Patipol_H.py
@@ -0,0 +1,31 @@
+
+
+usernameInput = input("Username: ")
+password = input("Password: ")
+
+if usernameInput == ("patipol") and password == ("7979"):
+    print("Welcome to",usernameInput,"!")
+    print(" สินค้า                      ราคา")
+    print("1.Ram      :         2,000 THB")
+    print("2.Mouse    :           250 THB")
+    print("3.Keyboard :           599 THB")
+
+    usernameInput = int(input("เลขสินค้าที่ต้องการ : "))
+    if usernameInput == 1:
+        print("Ram")
+        Ram = int(input("จำนวนที่ต้องการ: "))
+        price = int(2000)
+        result = Ram * price
+        print(result)
+    elif usernameInput == 2:
+        print("Mouse")
+        Mouse = int(input("จำนวนที่ต้องการ: "))
+        price = int(250)
+        result = Mouse * price
+        print(result)
+    elif usernameInput == 3:
+        print("Keyboard")
+        Keyboard = int(input("จำนวนที่ต้องการ: "))
+        price = int(599)
+        result = Keyboard * price
+        print(result)
