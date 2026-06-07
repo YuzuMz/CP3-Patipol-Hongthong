150db4b444d8b3219394fa59207708b8f53dfabd
YuzuMz <patipolz.15@gmail.com>
2026-06-07 15:53:42 +0700

    Add files via upload

---
+++ Assignments/Lecture50_Patipol_H.py
@@ -0,0 +1,19 @@
+def addNumber1(x, y):
+    print(x + y)
+
+addNumber1(1, 2)
+
+def addNumber2(x, y):
+    print(x - y)
+
+addNumber2(5, 2)
+
+def addNumber3(x, y):
+    print(x * y)
+
+addNumber3(1, 2)
+
+def addNumber4(x, y):
+    print(x / y)
+
+addNumber4(4, 2)
