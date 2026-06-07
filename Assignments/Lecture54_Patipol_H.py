f2f6ddfcfb2df3a8f5f516e30554ddac64eeaf5d
YuzuMz <patipolz.15@gmail.com>
2026-06-07 20:07:16 +0700

    Add files via upload

---
+++ Assignments/Lecture54_Patipol_H.py
@@ -0,0 +1,36 @@
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return True
+    else:
+        print("Error")
+
+
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+    return menuSelect()
+
+def menuSelect():
+    userSelected = int(input(">>"))
+    if userSelected == 1:
+        return vatCalculator(int(input("Total Price : ")))
+    if userSelected == 2:
+        return priceCalculator()
+
+def vatCalculator(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+
+def priceCalculator():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculator(price1 + price2)
+
+
+if login():
+    print(showMenu())
+
